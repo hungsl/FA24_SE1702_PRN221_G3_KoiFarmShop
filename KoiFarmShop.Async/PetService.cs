e4aeffe981aa46e90ab32fb627091c68aae22f35
DESKTOP-68QPN1I\ASUS <offunikeynt@gmail.com>
2024-10-24 12:46:04 +0700

    async

---
+++ KoiFarmShop.Async/PetService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoiFarmShop.Async
+{
+    public class PetService
+    {
+        public string Name { get; set; }
+        public string Duration { get; set; }
+        public DateTime AvailableTo { get; set; }
+    }
+}
